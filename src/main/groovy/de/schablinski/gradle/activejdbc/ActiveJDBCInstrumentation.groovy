e52358ab4f4300de6eefd77de852e85668c6c762
cschabl <biz@schablinski.de>
2019-05-31 07:04:59 +0200

    unused import removed

--- src/main/groovy/de/schablinski/gradle/activejdbc/ActiveJDBCInstrumentation.groovy
+++ src/main/groovy/de/schablinski/gradle/activejdbc/ActiveJDBCInstrumentation.groovy
@@ -16,7 +16,6 @@
 package de.schablinski.gradle.activejdbc
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.TaskAction
